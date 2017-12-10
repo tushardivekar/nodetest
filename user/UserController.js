7944740e8deb451ba7cd1150633f7df5abdab8d7
Tushar Divekar <tush.div@gmail.com>
2017-12-10 01:56:02 -0800

    added user route and database connection

---
+++ user/UserController.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+
+var User = require('./User');
+
+module.exports = router;
